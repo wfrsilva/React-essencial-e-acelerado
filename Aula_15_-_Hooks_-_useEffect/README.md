ae3677af016237af946683221e2e86426dd41155
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2024-06-11 22:19:15 -0300

    Aula 15 concluida

--- Aula_15_-_Hooks_-_useEffect/README.md
+++ Aula_15_-_Hooks_-_useEffect/README.md
@@ -1,11 +1,13 @@
 # Aula 15 - Hooks - useEffect - React essencial e acelerado
 
-
+## Introdução
 > O presente vídeo continua a apresentação dos Hooks, que são funcões previamente definidas na biblioteca React. Especificamenmte, apresenta o hook useEffect e como ele possibilita o tratamento de efeitos colaterais. Corresponde ao conteúdo da seção 5.1, do capítulo 5 do livro 'React essencial e acelerado'.
 [Professor Ricardo Pereira e Silva - Descrição Aula 15](https://www.youtube.com/watch?v=17adf88_fe0&ab_channel=ModelagemOrientadaaObjetoscomUML)
 
 ## Tela do App
 
+## Sugestões para versão 2.0
+- App.jsx - linhas 11 a 17 - ```}else{if(count < 0){```- Poderia refatorar? 
 
 ## Links
 - [Video-aulas React essencial e acelerado](https://www.youtube.com/watch?v=Wdto4xO981g&list=PLQb3t1uw-rpFCJlfyglJkJ_8Zz_gMIa7A&index=1&ab_channel=ModelagemOrientadaaObjetoscomUML);
--- Aula_15_-_Hooks_-_useEffect/src/App.jsx
+++ Aula_15_-_Hooks_-_useEffect/src/App.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [count, setCount]= useState(0);
+  const[description, setDescription]= useState('nulo');
+
+  useEffect(()=> {
+    let newDescription = ''
+    if (count > 0){
+      newDescription = 'positivo'
+    }else{
+      if(count < 0){
+        newDescription = 'negativo'
+      }else{
+        newDescription = 'nulo'
+      }
+    }
+    setDescription(newDescription)
+  }, [count])
+
+  function increment() {
+    setCount(value => value + 1)
+  }
+
+  function decrement(){
+    setCount(value => value - 1)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Exemplo useEffect</h1>
+      <h1>Valor: {count}</h1>
+      <h2>O valor é {description}</h2>
+      <button onClick={increment}>incrementar</button>
+      <button onClick={decrement}>decrementar</button>
+    </div>
   )
 }
 
