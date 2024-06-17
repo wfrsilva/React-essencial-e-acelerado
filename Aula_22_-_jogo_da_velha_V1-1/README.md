10f39e3bbf4fc782d53f7d6848276171ace3965b
Wendel (CIGA) Silva <wfrsilva@gmail.com>
2024-06-17 13:07:25 -0300

    jogo da velha - Versão 1.1 - atualizado readme
    - Usar ```async/await``` em vez de ```.then()```: Isso torna o código mais fácil de ler e entender.
    - Funções Auxiliares Fora do Componente:
        - As funções ```generateId``` e ```getUserId``` para fora do componente App.
    - Simplificação do useState:
        - Removida as funções anônimas dos ```useState``` porque não são necessárias.
    - Tratamento de Erros:
        - Adicionados blocos ```try/catch``` para tratar erros nas chamadas de API.

--- Aula_22_-_jogo_da_velha_V1-1/README.md
+++ Aula_22_-_jogo_da_velha_V1-1/README.md
@@ -1,8 +1,40 @@
-# React + Vite
+# Aula 22 - jogo da velha - Versão 1.1
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Introdução
+> Esta videoaula é a quinta de uma série de oito aulas, voltada a apresentar o desenvolvimento de uma aplicação do mundo real, um jogo da velha, fazendo uso dos conhecimentos das aulas anteriores. O presente vídeo é o primeiro (de três) que trata a implementação da aplicação. Corresponde ao conteúdo da seção 7.5 do capítulo 7 do livro 'React essencial e acelerado'.
+[Professor Ricardo Pereira e Silva - Descrição Aula 22](https://www.youtube.com/watch?v=1rp27bprtQw&list=PLQb3t1uw-rpFCJlfyglJkJ_8Zz_gMIa7A&index=22&ab_channel=ModelagemOrientadaaObjetoscomUML)
 
-Currently, two official plugins are available:
+# Aula 24 - O exemplo jogo da velha - código 3 - React essencial e acelerado
+> Esta videoaula é a sétima de uma série de oito aulas, voltada a apresentar o desenvolvimento de uma aplicação do mundo real, um jogo da velha, fazendo uso dos conhecimentos das aulas anteriores. O presente vídeo é o terceiro (de três) que trata a implementação da aplicação. Corresponde ao conteúdo da seção 7.5 do capítulo 7 do livro 'React essencial e acelerado'.
+[Professor Ricardo Pereira e Silva - Descrição Aula 24](https://www.youtube.com/watch?v=7obIdrXjEVQ&ab_channel=ModelagemOrientadaaObjetoscomUML)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Tela do App
+![image](https://github.com/wfrsilva/React-essencial-e-acelerado/assets/8933834/5399830e-4158-403b-bef8-3a63732ea3d1)
+
+
+
+## Versão 1.1
+Alterações, melhorias e complementos adicionados que não estão presentes na videoaula nem no repositório do professor.
+- Icone [jogo-velha.svg](https://github.com/wfrsilva/React-essencial-e-acelerado/blob/main/Aula_22_-_O_exemplo_jogo_da_velha/public/jogo-velha.svg)
+- Diretorio [documents](https://github.com/wfrsilva/React-essencial-e-acelerado/tree/main/Aula_22_-_O_exemplo_jogo_da_velha/src/documents)/
+    - Diagrama [Jogo-Velha-Componentes.svg](https://raw.githubusercontent.com/wfrsilva/React-essencial-e-acelerado/7403e80ff1ce9fc7628d0f390765f1bafe1b427b/Aula_22_-_O_exemplo_jogo_da_velha/src/documents/Jogo-Velha-Componentes.svg)
+- Usar ```async/await``` em vez de ```.then()```: Isso torna o código mais fácil de ler e entender.
+- Funções Auxiliares Fora do Componente:
+    - As funções ```generateId``` e ```getUserId``` para fora do componente App.
+- Simplificação do useState:
+    - Removida as funções anônimas dos ```useState``` porque não são necessárias.
+- Tratamento de Erros:
+    - Adicionados blocos ```try/catch``` para tratar erros nas chamadas de API.
+
+ ### [jogo-velha.svg](https://github.com/wfrsilva/React-essencial-e-acelerado/blob/main/Aula_22_-_O_exemplo_jogo_da_velha/public/jogo-velha.svg)
+ 
+ <img src="https://github.com/wfrsilva/React-essencial-e-acelerado/blob/main/Aula_22_-_O_exemplo_jogo_da_velha/public/jogo-velha.svg" alt="Logo jogo da Velha" width="75" height="75">
+
+### Diagrama [Jogo-Velha-Componentes.svg](https://raw.githubusercontent.com/wfrsilva/React-essencial-e-acelerado/7403e80ff1ce9fc7628d0f390765f1bafe1b427b/Aula_22_-_O_exemplo_jogo_da_velha/src/documents/Jogo-Velha-Componentes.svg)
+
+<img src="https://raw.githubusercontent.com/wfrsilva/React-essencial-e-acelerado/7403e80ff1ce9fc7628d0f390765f1bafe1b427b/Aula_22_-_O_exemplo_jogo_da_velha/src/documents/Jogo-Velha-Componentes.svg" alt="Diagrama Jogo-Velha-Componentes" style="max-width: 100%;">
+
+
+## Links
+- [Video-aulas React essencial e acelerado](https://www.youtube.com/watch?v=Wdto4xO981g&list=PLQb3t1uw-rpFCJlfyglJkJ_8Zz_gMIa7A&index=1&ab_channel=ModelagemOrientadaaObjetoscomUML);
+- [Livro React essencial e acelerado](https://www.amazon.com.br/React-essencial-acelerado-Ricardo-Pereira-ebook/dp/B0CS4MT24K/ref=sr_1_1?crid=KYMXD3Y1SRYA&keywords=react+essencial+e+acelerado&qid=1705929956&sprefix=react+essen%2Caps%2C191&sr=8-1);
--- Aula_22_-_jogo_da_velha_V1-1/src/App.jsx
+++ Aula_22_-_jogo_da_velha_V1-1/src/App.jsx
@@ -1,71 +1,79 @@
-import { useEffect, useState } from 'react'
-import GameHeader from './components/GameHeader'
-import Board from './components/Board'
-import GameMessage from './components/GameMessage'
-import './app.css'
-import axios from "axios"
-import jogoVelhaLogo from '/jogo-velha.svg'
+import { useEffect, useState, memo } from 'react';
+import GameHeader from './components/GameHeader';
+import Board from './components/Board';
+import GameMessage from './components/GameMessage';
+import './app.css';
+import axios from 'axios';
+import jogoVelhaLogo from '/jogo-velha.svg';
+
+function generateId() {
+  let d = new Date();
+  let aTime = d.getTime();
+  let timeString = aTime.toString(10);
+  let truncatedTimeString = timeString.slice(6, 13);
+  let newTempIdValue = parseInt(truncatedTimeString);
+  return newTempIdValue;
+}
+
+function getUserId() {
+  let jvLocalId = localStorage.getItem('jv_local_id');
+  if (jvLocalId == null) {
+    jvLocalId = generateId();
+    localStorage.setItem('jv_local_id', jvLocalId);
+  }
+  return jvLocalId;
+}
 
 function App() {
-  const headerText = ["Jogo da Velha", "(Dificuldade: Kobayashi Maru)"]
-  const[gameMessage, setGameMessage] = useState(()=> { return "Carregando..."})
-  const[gameMatrix, setGameMatrix] = useState(()=> { return [[0,1,0],[1,2,1],[0,1,0]]})
-  const[url,setUrl] = useState("")
+  const headerText = ['Jogo da Velha', '(Dificuldade: Kobayashi Maru)'];
+  const [gameMessage, setGameMessage] = useState('Carregando...');
+  const [gameMatrix, setGameMatrix] = useState([[0, 1, 0], [1, 2, 1], [0, 1, 0]]);
+  const [url, setUrl] = useState('');
 
-  function generateId(){
-    let d = new Date();
-    let aTime = d.getTime();
-    let timeString = aTime.toString(10);
-    let truncatedTimeString = timeString.slice(6,13);
-    let newTempIdValue = parseInt(truncatedTimeString);
-    
-    return newTempIdValue;
-  }
+  useEffect(() => {
+    const userId = getUserId();
+    const gameUrl = `https://dogserver.pythonanywhere.com/${userId}/`;
+    setUrl(gameUrl);
+  }, []);
 
-  function get_userId(){
-    let jv_local_id = localStorage.getItem("jv_local_id");
-    
-    if(jv_local_id == null){
-      jv_local_id = generateId();
-      localStorage.setItem("jv_local_id", jv_local_id);
+  useEffect(() => {
+    const fetchGameState = async () => {
+      try {
+        const response = await axios.get(url);
+        setGameMessage(response.data[0]);
+        setGameMatrix(response.data[1][0]);
+      } catch (error) {
+        console.error('Erro ao buscar o estado do jogo:', error);
+      }
+    };
+    if (url) {
+      fetchGameState();
     }
+  }, [url]);
 
-    return jv_local_id;
-  }
-
-  useEffect(()=> {
-    const id = get_userId()
-    const url = "https://dogserver.pythonanywhere.com/" + id + "/"
-    setUrl(url)
-  },[])
-
-  useEffect(()=> {
-    if(url){
-      axios.get(url)
-        .then(gameState => {
-          setGameMessage(gameState.data[0])
-          setGameMatrix(gameState.data[1][0])
-        })
+  const handlePositionClick = async (id) => {
+    const [line, column] = id;
+    const data = `line=${line + 1}&column=${column + 1}`;
+    try {
+      const response = await axios.post(url, data);
+      setGameMessage(response.data[0]);
+      setGameMatrix(response.data[1][0]);
+    } catch (error) {
+      console.error('Erro ao atualizar o estado do jogo:', error);
     }
-  },[url])
+  };
 
-  function handlePositionClick(id){
-    const[line,column] = id;
-    const data = 'line='+(line+1)+'&column='+(column+1);
-    axios.post(url,data)
-      .then(gameState => {
-        setGameMessage(gameState.data[0])
-        setGameMatrix(gameState.data[1][0])
-      });
-  }
+  const MemoizedGameHeader = memo(GameHeader);
+  const MemoizedBoard = memo(Board);
+  const MemoizedGameMessage = memo(GameMessage);
 
   return (
     <div className='container'>
-    <GameHeader title={headerText} />
-    <Board matrix={gameMatrix} onPositionClick={handlePositionClick} />
-    <GameMessage message={gameMessage} />
+      <MemoizedGameHeader title={headerText} />
+      <MemoizedBoard matrix={gameMatrix} onPositionClick={handlePositionClick} />
+      <MemoizedGameMessage message={gameMessage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
